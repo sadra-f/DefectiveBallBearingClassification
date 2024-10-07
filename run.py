53f254a6b84608d4f91609bfc4095bdcb76fbe88
sadra-f <sadrafouladi@gmail.com>
2024-10-07 18:00:03 +0330

    added code to create ok-def models from dataset

--- run.py
+++ run.py
@@ -1,15 +1,34 @@
 from Classification.Classifier import HOGBasedClassifier as HBC
-from statics.Paths import def_model_path, ok_model_path, test_def_path, test_ok_path
+from statics.Paths import *
 from Helpers.IO import read_array, read_imgs_in_path
 from itertools import chain
-
+# from Preprocessing.Vectorizer import vectorize_dataset
+# from Clustering.CKMeans import cluster_vectors, mean_of_clusters
+# from Helpers.IO import write_array 
 def main():
+    # def_dataset = read_imgs_in_path(train_def_path)
+    # ok_dataset = read_imgs_in_path(train_ok_path)
+
+    # def_dataset = vectorize_dataset(def_dataset)
+    # ok_dataset = vectorize_dataset(ok_dataset)
+
+    # def_dataset = cluster_vectors(def_dataset)
+    # ok_dataset = cluster_vectors(ok_dataset)
+
+    # def_dataset = mean_of_clusters(def_dataset)
+    # ok_dataset = mean_of_clusters(ok_dataset)
+
+    # write_array(def_dataset, def_model_path)
+    # write_array(ok_dataset, ok_model_path)
+
     classifier = HBC()
     ok_model = read_array(ok_model_path)
     def_model = read_array(def_model_path)
     classifier.change_models(ok_model, def_model)
     tests = read_imgs_in_path(test_ok_path)
     tests2 = read_imgs_in_path(test_def_path)
+    for val in tests:
+        tests2.append(val)
     res = classifier.predict(tests2)
     print(res)
 
