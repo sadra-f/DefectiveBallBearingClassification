69811df4482520e37c6d4c060fe4770325dc54ca
sadra-f <sadrafouladi@gmail.com>
2024-10-11 21:38:01 +0330

    added class name to viewing of the img

--- Helpers/ImgView.py
+++ Helpers/ImgView.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 
-def display_images(original, lbp_image):
+def display_images(original, lbp_image, cls:{"ok","defective"}):
     plt.figure(figsize=(10, 5))
 
     # Original image
     plt.subplot(1, 2, 1)
     plt.imshow(original, cmap='gray')
-    plt.title('Original Image')
+    plt.title(f"Original {cls} Image")
     plt.axis('off')
 
     # LBP image
     plt.subplot(1, 2, 2)
     plt.imshow(lbp_image, cmap='gray')
-    plt.title('HOG Image')
+    plt.title(f"HOG {cls} Image")
     plt.axis('off')
 
     plt.show()
